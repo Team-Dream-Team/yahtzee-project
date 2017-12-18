57fed72ad0c0ac043b8d87a7bf7815b4f98d96f5
Ben <bzhao18@github.com>
2017-12-18 16:46:44 -0500

    score changes

--- Player.py
+++ Player.py
@@ -17,89 +17,89 @@ class Player:
 			print(dice)	
 		return dice
 	#check dice list for given number and add them all up
-	def checkOne(): 
+	def checkOne(score1): 
 		if self.dice.count(1) > 0:
-			self.score += self.dice.count(1)*1
+			self.score1 += self.dice.count(1)*1
 		else: 
-			self.score += 0
-	def checkTwo(): 
+			self.score1 += 0
+	def checkTwo(score2): 
 		if self.dice.count(2) > 0:
-			self.score += self.dice.count(2)*2
+			self.score2 += self.dice.count(2)*2
 		else: 
-			self.score += 0
-	def checkThree(): 
+			self.score2 += 0
+	def checkThree(score3): 
 		if self.dice.count(3) > 0:	
-			self.score += self.dice.count(3)*3
+			self.score3 += self.dice.count(3)*3
 		else: 
-			self.score += 0
-	def checkFour(): 
+			self.score3 += 0
+	def checkFour(score4): 
 		if self.dice.count(4) > 0:
-			self.score += self.dice.count(4)*4
+			self.score4 += self.dice.count(4)*4
 		else: 
-			self.score += 0
-	def checkFive(): 
+			self.score4 += 0
+	def checkFive(score5): 
 		if self.dice.count(5) > 0:
-			self.score += self.dice.count(5)*5
+			self.score5 += self.dice.count(5)*5
 		else: 
-			self.score += 0
-	def checkSix(): 
+			self.score5 += 0
+	def checkSix(score6): 
 		if self.dice.count(1) > 0:
-			self.score += self.dice.count(6)*6
+			self.score6 += self.dice.count(6)*6
 		else: 
-			self.score += 0
+			self.score6 += 0
 	#check three of a kind
-	def checkthreeofakind():
+	def checkthreeofakind(scorethreeofakind):
 	#add up all the dice in the list if there is at least a three of a kind
 		for number in dice: 
 			if dice.count(number) >= 3: 
-				self.score += sum(dice)
+				self.scorethreeofakind += sum(dice)
 			else: 
-				self.score += 0
+				self.scorethreeofakind += 0
 
-	def checkfourofakind(): 
+	def checkfourofakind(scorefourofakind): 
 	#add up all the dice if there is at least a four of a kind
 		for number in dice: 
 			if dice.count(number) >= 4: 
-				self.score += sum(dice)
+				self.scorefourofakind += sum(dice)
 			else:
-				self.score += 0
+				self.scorefourofakind += 0
 
-	def checkYahtzee():
+	def checkYahtzee(scoreYahtzee):
 	#if there is a yahtzee, you will be awarded points based off your first or second time
 		for number in dice: 
 			if dice.count(number) == 5: 
 				if yahtzeeWin:
-					self.score += 100
+					self.scoreYahtzee += 100
 				else:
 					yahtzeeWin = True
-					self.score += 50
+					self.scoreYahtzee += 50
 			else: 
-				self.score += 0
+				self.scoreYahtzee += 0
 
 	#check for full house numbers in the dice and award 25 points if full house exists
 	#otherwise, award 0 points
-	def checkFullHouse():
+	def checkFullHouse(scoreFullHouse):
 		for num in dice: 
 			if dice.count(num) == 3:
 				for second_num in dice: 
 					if dice.count(second_num) == 2:
-						self.score += 25
+						self.scoreFullHouse += 25
 			else: 
-				self.score += 0
+				self.scoreFullHouse += 0
 
 	
-	def small_straight():
+	def small_straight(scoresmall_straight):
 		sortArray = list(set(dice))
 		if sortArray == [1,2,3,4] or sortArray == [2,3,4,5] or sortArray == [3,4,5,6]:
-			self.score += 30
+			self.scoresmall_straight += 30
 		else: 
-			self.score += 0
-	def large_straight():
+			self.scoresmall_straight += 0
+	def large_straight(scorelarge_straight):
 		sortArray = list(set(dice))
 		if sortArray == [1,2,3,4,5] or sortArray == [2,3,4,5,6]:
-			self.score += 40
+			self.scorelarge_straight += 40
 		else: 
-			self.score += 0
+			self.scorelarge_straight += 0
 
 
 
